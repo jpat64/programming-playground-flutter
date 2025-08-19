22b7f6355e41f9419173dba339925683126909c9
jpat64 <jimmy@pattersonadvice.com>
2025-08-19 14:06:52 -0500

    rename folder and restructure home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:programming_playground/services/HomeScreen.dart';
+import 'package:programming_playground/screens/HomeScreen.dart';
 
 void main() {
   runApp(const Playground());
--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -12,14 +12,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,18 +23,9 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const Text("Welcome to the Playground Home Screen!"),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
