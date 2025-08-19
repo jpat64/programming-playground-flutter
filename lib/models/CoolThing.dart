4db1527fc47008024221388013b6dc5e674afdca
jpat64 <jimmy@pattersonadvice.com>
2025-08-19 14:22:46 -0500

    cool thing model

---
+++ lib/models/CoolThing.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: file_names
+
+class CoolThing {
+  final String name;
+  final int id;
+
+  const CoolThing({required this.name, required this.id});
+}
--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:programming_playground/models/CoolThing.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.title});
@@ -12,6 +13,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  CoolThing? thing;
+
+  @override
+  void initState() {
+    super.initState();
+    thing = CoolThing(id: 0, name: "The Coolest Thing");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +33,14 @@ class _HomeScreenState extends State<HomeScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text("Welcome to the Playground Home Screen!"),
+            Card(
+              color: Colors.deepPurple[200],
+              child: Text(
+                thing != null
+                    ? "${thing!.name} (id: ${thing!.id})"
+                    : "No cool things yet.",
+              ),
+            ),
           ],
         ),
       ),
